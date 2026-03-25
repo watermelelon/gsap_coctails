f3d13bbb9460f0d2e03021f2df1eea1cec623c5b
Emilio <emiliodaniel02@hotmail.com>
2026-03-24 19:24:15 -0500

    Added cocktail info

---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import {cocktailLists, mockTailLists} from "../../constants/index.js";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+
+function Cocktails() {
+
+
+    useGSAP(() =>{
+        const parallaxTimeline = gsap.timeline({
+            scrollTrigger:{
+                trigger: '#cocktails',
+                start: 'top 30%',
+                end: 'bottom 80%',
+                scrub:true,
+            }
+        })
+        parallaxTimeline
+            .from('#c-left-leaf',{
+                x: -100, y: 100
+            } )
+
+            .from('#c-right-leaf',{
+                x: 100, y: 100
+            } )
+
+    })
+
+
+    return (
+        <section id="cocktails" className="noisy">
+            <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
+            <img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
+
+            <div className="list">
+                <div className="popular">
+                    <h2>Most Popular Cocktails:</h2>
+                    <ul>
+                        {cocktailLists.map(({name, country, detail,price})=>(
+                            <li key={name}>
+                                <div className = "md:me-28">
+                                    <h3>{name}</h3>
+                                    <p>{country} | {detail}</p>
+                                </div>
+                                <span>- {price}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+
+                <div className="loved">
+                    <h2>Most Loved Mocktails:</h2>
+                    <ul>
+                        {mockTailLists.map(({name, country, detail,price})=>(
+                            <li key={name}>
+                                <div className = "md:me-28">
+                                    <h3>{name}</h3>
+                                    <p>{country} | {detail}</p>
+                                </div>
+                                <span>- {price}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Cocktails
